765afbb009608253e8215bcf152e016255e417ca
softmarshmallow <woojoo@softmarshmallow.com>
2020-11-10 03:24:55 +0900

    added quicklook query params as type

--- lib/projects/quicklook/index.ts
+++ lib/projects/quicklook/index.ts
@@ -1,3 +1,4 @@
+import { encodeQueryData } from "lib/utils/url.utils";
 
 interface QuickLookRequestFlutter {
     flutterCode: string
@@ -5,6 +6,24 @@ interface QuickLookRequestFlutter {
 
 interface QuickLookResponse {
     url: string
+}
+
+export type framework = "flutter" | "react"
+export type language = "dart" | "js"
+
+export interface QuicklookQueryParams {
+    id: string
+    framework: framework
+    language: language
+    url?: string
+    name: string
+    w?: number
+    h?: number
+}
+
+export function buildConsoleQuicklookUrl(props: QuicklookQueryParams) {
+    const querystring = encodeQueryData(props);
+    return `https://console.bridged.xyz/quicklook?${querystring}`;
 }
 
 export function quickLookFlutterDCC(args: QuickLookRequestFlutter): QuickLookResponse {
---
+++ lib/utils/url.utils.ts
@@ -0,0 +1,7 @@
+// https://stackoverflow.com/a/111545/5463235
+export function encodeQueryData(data) {
+    const ret = [];
+    for (let d in data)
+        ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
+    return ret.join('&');
+}
