7af55cedd9292377cdfa56711762c53136499f12
softmarshmallow <woojoo@softmarshmallow.com>
2020-11-17 12:30:41 +0900

    added icon config to types

---
+++ lib/types/icon/index.ts
@@ -0,0 +1,7 @@
+
+export interface IconConfig {
+    default_size: string
+    variant: "outlined" | "outlined" | "twotone" | "default" | "default" | "sharp"
+    family?: string
+    host: "material" | "reflect" | string
+}
