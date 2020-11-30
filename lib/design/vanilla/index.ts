b33923cfc8a0a0c0b21ee233853b3a1fad4e2983
softmarshmallow <woojoo@softmarshmallow.com>
2020-11-30 15:23:42 +0900

    removed vanilla element type

--- lib/design/vanilla/index.ts
+++ lib/design/vanilla/index.ts
@@ -1,9 +1,1 @@
-import { TextManifest, ImageManifest } from "@reflect.bridged.xyz/core"
-import { TransportLayer } from "./vanilla-transport"
-
-/**
- * Vanilla Element type alias. can be text or image at this time.
- */
-export type VanilaElement = TransportLayer<TextManifest> | TransportLayer<ImageManifest>
-
 export * from "./vanilla-transport"
