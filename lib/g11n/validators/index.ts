7749c63d5f3a4b984d40c2ce955456bffa98537f
softmarshmallow <woojoo@softmarshmallow.com>
2020-12-09 17:11:10 +0900

    added placeholder name validation boilerplate

--- lib/g11n/validators/index.ts
+++ lib/g11n/validators/index.ts
@@ -1,1 +1,16 @@
-export * from "./key-name.validate"
+
+/**
+ * validates if key contains any suspecious, or non-maintanable key format.
+ * @param v 
+ */
+export function keyNameFormatValidation(v: string): boolean {
+    // TODO implement
+    throw 'keyNameFormatValidation not implemented'
+}
+
+/**
+ * validates if placeholder name e.g. "USER_AGE" is a valid char sequence to be placeholder.
+ */
+export function placeholderNameFormatValidation(v: string): boolean {
+    throw 'placeholderNameFormatValidation not implemented'
+}
--- lib/g11n/validators/key-name.validate.ts
+++
@@ -1,8 +0,0 @@
-/**
- * validates if key contains any suspecious, or non-maintanable key format.
- * @param v 
- */
-export function keyNameFormatValidation(v: string): boolean {
-    // TODO implement
-    throw 'keyNameFormatValidation not implemented'
-}
