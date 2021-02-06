05c00dc411fbe8d73ab9667443d8aed515f98391
softmarshmallow <woojoo@softmarshmallow.com>
2021-02-07 04:31:51 +0900

    updated cors

--- lib/cors/index.ts
+++ lib/cors/index.ts
@@ -1,16 +1,21 @@
-import Axios from "axios";
+import Axios, { AxiosProxyConfig } from "axios";
 import { CORS_SERVICE_HOST } from "../constants/hosts";
 
 /**
  * A base axios object that holds cors.bridged.cc as base host
  */
 export const corsAxios = Axios.create({
-    baseURL: CORS_SERVICE_HOST
-})
+    baseURL: CORS_SERVICE_HOST,
+});
+
+export const corsAxiosProxyConfig: AxiosProxyConfig = {
+    host: CORS_SERVICE_HOST,
+    port: 443,
+};
 
 /**
  * builds cors free request url with givvne input.
  */
-export function buildCorsFreeUrl(url: string): string{
-    return `${CORS_SERVICE_HOST}/${url}`
+export function buildCorsFreeUrl(url: string): string {
+    return `${CORS_SERVICE_HOST}/${url}`;
 }
