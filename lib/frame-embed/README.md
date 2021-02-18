c3b9edc4d74fc85c371e698873ddfa170fba1ae9
softmarshmallow <woojoo@softmarshmallow.com>
2021-02-19 00:48:12 +0900

    add flutter frame types & frame url builder

---
+++ lib/frame-embed/README.md
@@ -0,0 +1,3 @@
+# Frame Embed
+
+-   learn more at [frame.appbox.bridged.xyz github](https://github.com/bridgedxyz/appbox/tree/main/packages/frames)
---
+++ lib/frame-embed/flutter.ts
@@ -0,0 +1,33 @@
+const HOST_APPBOX_FRAME_FLUTTER = "https://frames-appbox.vercel.app/flutter";
+/**
+ * state of flutter frame loading
+ */
+export type FlutterLoadingState =
+    | "pre-warming"
+    | "compiling"
+    | "js-compiled"
+    | "engine-loaded"
+    | "drawing"
+    | "complete"
+    | "failed";
+
+export interface FlutterFrameQuery {
+    id?: string;
+    src: string;
+    mode: "content" | "url";
+    language: FlutterCompatLanguage;
+}
+
+export type FlutterCompatLanguage = "js" | "dart";
+
+/**
+ *
+ * @param params
+ * @param host optional override host. should end withou
+ */
+export function buildFlutterFrameUrl(params: FlutterFrameQuery, host?: string) {
+    const query = new URLSearchParams({ ...params } as Record<string, string>);
+
+    // use default host if no override "host" param givven
+    return `${host ?? HOST_APPBOX_FRAME_FLUTTER}?${query.toString()}`;
+}
