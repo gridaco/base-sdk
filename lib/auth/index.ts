41de7c6dd23db356f53a97532c597d9eaa1ac9f3
GwonHeeJun <developer_gwon@naver.com>
2021-03-11 17:08:45 +0900

    modify : define type & string combination

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -5,12 +5,12 @@ const ACCOUNT_SERVICE_XYZ = "https://accounts.bridged.xyz?";
 export function createAuthenticationUrl(
     parameter: CreateAuthenticationUrlParams
 ) {
-    let accountServiceParam;
+    let accountServiceParam: string = "";
     if (parameter.redirect_uri != null) {
-        accountServiceParam = `redirect_uri=${parameter.redirect_uri}&`;
+        accountServiceParam += `redirect_uri=${parameter.redirect_uri}&`;
     }
     if (parameter.request_session_id != null) {
-        accountServiceParam = `request_session_id=${parameter.request_session_id}`;
+        accountServiceParam += `request_session_id=${parameter.request_session_id}`;
     }
     return ACCOUNT_SERVICE_XYZ + accountServiceParam;
 }
