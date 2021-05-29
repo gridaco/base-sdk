d74a9fb7843264836e3bc69ce2fe9b48dc6146b2
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-30 03:54:31 +0900

    cdir auth to auth-first-party

--- core/constants/hosts.ts
+++ core/constants/hosts.ts
@@ -8,5 +8,20 @@ export const LEGACY_SCENE_STORE_SERVICE_HOST = "https://scene-store.bridged.cc";
 export const G11N_SERVICE_HOST = "https://g11n.bridged.cc";
 export const ASSET_SERVICE_HOST = "https://assets.bridged.cc";
 export const CORS_SERVICE_HOST = "https://cors.bridged.cc";
+
+///
+/// region internal services
+/// only the host information is public. source is closed.
+///
+
 export const INTERNAL_PAY_WEB_HOST = "https://pay.bridged.xyz";
 export const INTERNAL_ACCOUNTS_WEB_HOST = "https://accounts.bridged.xyz";
+
+/**
+ * secure host specifically designed for usage with accounts.bridged.xyz (same domain)
+ * only the host information is public. source is closed.
+ */
+export const INTERNAL_SECURE_ACCOUNTS_SERVICE_HOST =
+    "https://accounts.services.bridged.xyz";
+
+export const INTERNAL_WS_PROXY_AUTH_HOST = "wss://ws.proxy.auth.bridged.cc";
