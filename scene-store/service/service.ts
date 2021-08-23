6c8dc0b9320bb90358cdc8a6fdb6883379a7075f
softmarshmallow <woojoo@softmarshmallow.com>
2021-08-24 05:43:41 +0900

    add automatic otp loader

--- scene-store/service/service.ts
+++ scene-store/service/service.ts
@@ -1,17 +1,57 @@
+import Axios from "axios";
 import { __HOSTS } from "@base-sdk/core";
 import * as api from "../__api";
+import { _configure } from "../__api";
+
+interface StaticToken {
+    type: "token";
+    token: string;
+}
+
+interface BrowserAuthOtp {
+    type: "auto-browser-otp";
+    token?: () => Promise<string>;
+}
+
+const DEFAULT_OTP_LOADER = async () => {
+    const _r = await Axios.create({
+        baseURL: "https://accounts.services.grida.co/issue/otp",
+        withCredentials: true,
+    }).get<{
+        user_id: string;
+        access_token: string;
+    }>("/");
+
+    return _r.data.access_token;
+};
+
+type CredentialProvider = StaticToken | BrowserAuthOtp;
 
 export class SceneStoreService {
-    constructor() {}
+    constructor(private readonly credential: CredentialProvider) {}
+
+    private async configure() {
+        switch (this.credential.type) {
+            case "auto-browser-otp": // this otp must be issued on every request.
+                const act = await (
+                    this.credential.token ?? DEFAULT_OTP_LOADER
+                )();
+                _configure(act);
+            case "token":
+                _configure((this.credential as StaticToken).token);
+        }
+    }
 
     async register(
         request: api.SceneRegisterRequest
     ): Promise<api.GetSceneRecordResult> {
+        await this.configure();
         const resp = await api._api_registerScene(request);
         return resp;
     }
 
     async get(id: string): Promise<api.GetSceneRecordResult> {
+        await this.configure();
         return await api._api_getScene(id);
     }
 
@@ -19,10 +59,12 @@ export class SceneStoreService {
         id: string,
         request: api.UpdateSharingPolicyRequest
     ): Promise<api.UpdateSharingPolicyResult> {
+        await this.configure();
         return await api._api_updateSharingPolicy(id, request);
     }
 
     async list() {
+        await this.configure();
         return await api._api_listMyScenes();
     }
 
