ca3306fbcb8fc271c38098a19f585cf34d67f089
softmarshmallow <woojoo@softmarshmallow.com>
2021-08-23 21:34:00 +0900

    scene types now deprecated and labeled "legacy"

--- base/design/vanilla/vanilla-transport.ts
+++ base/design/vanilla/vanilla-transport.ts
@@ -1,28 +1,27 @@
-import { IImageRepository } from "../../asset-repository"
-import { StorableLayer, StorableLayerData } from "../layers/storable-layer"
-import { StorableLayerType } from "../layers/storable-layer-type"
-import { StorableScene } from "../scenes"
+import { IImageRepository } from "../../asset-repository";
+import { StorableLayer, StorableLayerData } from "../layers/storable-layer";
+import { StorableLayerType } from "../layers/storable-layer-type";
+import { StorableScene } from "../legacy-scenes";
 
 /**
  * for transporting vanilla elements
  */
 export interface VanillaSceneTransport {
-    id: string
-    scene: StorableScene
-    repository: IImageRepository<any>
+    id: string;
+    scene: StorableScene;
+    repository: IImageRepository<any>;
 }
 
 /**
  * the single layer used for transportation as vanilla design
  */
 export interface TransportLayer extends StorableLayer {
-    nodeId: string
-    index: number
-    type: StorableLayerType
-    data: StorableLayerData
-    x: number
-    y: number
-    width: number
-    height: number
+    nodeId: string;
+    index: number;
+    type: StorableLayerType;
+    data: StorableLayerData;
+    x: number;
+    y: number;
+    width: number;
+    height: number;
 }
-
