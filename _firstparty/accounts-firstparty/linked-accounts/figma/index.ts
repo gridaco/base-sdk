918d4de7882413a8a883b92185a28eac15434cbf
softmarshmallow <woojoo@softmarshmallow.com>
2021-11-12 15:45:21 +0900

    add linked figma account interface

--- _firstparty/accounts-firstparty/linked-accounts/figma/index.ts
+++ _firstparty/accounts-firstparty/linked-accounts/figma/index.ts
@@ -1,6 +1,53 @@
 import { _BaseRestfulClient } from "@base-sdk-fp/core";
 
 /**
+ * A signature of figma account, via OAuth result, served by Grida Accounts Services
+ */
+export interface ILinkedFigmaAccount {
+    /**
+     * access token to access figma api
+     *
+     * e.g. "XtxiW-xxxxxxxxxxxxxxxxxxxx"
+     */
+    accessToken: string;
+    /**
+     * creation time
+     */
+    createdAt: Date;
+    /**
+     * expiry time
+     */
+    expiresAt: Date;
+    /**
+     * A session id.
+     *
+     * e.g."027cxxxx-xxxx-4e8f-xxxx-dadxxx8b0e18"
+     */
+    id: string;
+    /**
+     * e.g. "632943853725586700"
+     */
+    oAuthUserId: string;
+    /**
+     * provider
+     * @default "FIGMA"
+     */
+    provider: "FIGMA";
+    /**
+     * e.g. "xxxxxxxx-xxxxxxxxxx_--xxxxxxxxxxxxxxx"
+     */
+    refreshToken: string;
+    /**
+     * e.g. "2021-08-23T13:04:50.530Z"
+     */
+    updatedAt: Date;
+    /**
+     * e.g. "d4b5b27f-xxxx-4f89-b64c-xxxxxxxxxx"
+     */
+    userId: string;
+}
+
+/**
  * provide the client with `initClient`
  */
 export class FigmaLinkedAccountsClient {
@@ -15,9 +62,11 @@ export class FigmaLinkedAccountsClient {
      * returns the primary linked figma account
      * @returns
      */
-    async getPrimaryLinked() {
+    async getPrimaryLinked(): Promise<ILinkedFigmaAccount> {
         return (
-            await this.api.get(FigmaLinkedAccountsClient.basePath + "/primary")
+            await this.api.get<ILinkedFigmaAccount>(
+                FigmaLinkedAccountsClient.basePath + "/primary"
+            )
         ).data;
     }
 
@@ -25,16 +74,19 @@ export class FigmaLinkedAccountsClient {
      * returns all linked figma accounts
      * @returns
      */
-    async getLinkedAccounts() {
-        return (await this.api.get(FigmaLinkedAccountsClient.basePath + "/"))
-            .data;
+    async getLinkedAccounts(): Promise<ILinkedFigmaAccount[]> {
+        return (
+            await this.api.get<ILinkedFigmaAccount[]>(
+                FigmaLinkedAccountsClient.basePath + "/"
+            )
+        ).data;
     }
 
     /**
      * returns if there is any linked figma account.
      * @returns
      */
-    async hasLinkedFigmaAccount() {
+    async hasLinkedFigmaAccount(): Promise<boolean> {
         try {
             const r = await this.getPrimaryLinked();
             if (r !== undefined) {
