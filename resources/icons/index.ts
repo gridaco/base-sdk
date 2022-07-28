878c43ac0d458b0d67d2bdf686654363ee5832db
softmarshmallow <woojoo@softmarshmallow.com>
2022-07-28 21:51:02 +0900

    fix type

--- resources/icons/index.ts
+++ resources/icons/index.ts
@@ -1,15 +1,15 @@
-import { NamedIconConfig } from "@reflect-ui/core";
+import { IconConfig } from "@reflect-ui/core";
 import Axios from "axios";
 
 export async function loadSvg(
     key: string,
-    config: NamedIconConfig
+    config: IconConfig
 ): Promise<string> {
     const url = makeIconUrl(key, config);
     const raw = await (await Axios.get(url)).data;
     return raw;
 }
 
-export function makeIconUrl(name: string, config: NamedIconConfig): string {
+export function makeIconUrl(name: string, config: IconConfig): string {
     return `https://reflect-icons.s3-us-west-1.amazonaws.com/${config.host}/${name}.svg`;
 }
